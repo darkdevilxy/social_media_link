18ee08d79bf9e0443cf133cdeb2e667e472340e6
darkdevilxy <bhumagainxy@gmail.com>
2022-11-30 22:08:41 +1100

    On branch master
    Initial commit

---
+++ social_media_linker/src/App.tsx
@@ -0,0 +1,63 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ul>
+        <li>Content1</li>
+        <li>Content2</li>
+        <li>Content3</li>
+        <li>Content4</li>
+        <li>Content5</li>
+        <li>Content6</li>
+        <li>Content7</li>
+        <li>Content8</li>
+        <li>Content9</li>
+        <li>Content10</li>
+        <li>Content11</li>
+        <li>Content12</li>
+        <li>Content13</li>
+        <li>Content14</li>
+        <li>Content15</li>
+        <li>Content16</li>
+        <li>Content17</li>
+        <li>Content18</li>
+        <li>Content19</li>
+        <li>Content20</li>
+        <li>Content21</li>
+        <li>Content22</li>
+        <li>Content23</li>
+        <li>Content24</li>
+        <li>Content25</li>
+        <li>Content26</li>
+        <li>Content27</li>
+        <li>Content28</li>
+        <li>Content29</li>
+        <li>Content30</li>
+        <li>Content31</li>
+        <li>Content32</li>
+        <li>Content33</li>
+        <li>Content34</li>
+        <li>Content35</li>
+        <li>Content36</li>
+        <li>Content37</li>
+        <li>Content38</li>
+        <li>Content39</li>
+        <li>Content40</li>
+        <li>Content41</li>
+        <li>Content42</li>
+        <li>Content43</li>
+        <li>Content44</li>
+        <li>Content45</li>
+        <li>Content46</li>
+        <li>Content47</li>
+        <li>Content48</li>
+        <li>Content49</li>
+        <li>Content50</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
